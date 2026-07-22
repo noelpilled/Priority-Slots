636408134f88ddcc10477516bbca4bf0b892c6ea
noel <mail@noel.run>
2026-07-22 11:56:27 -0700

    cleanup

--- src/main/java/com/priorityslots/PrioritySlotsConfig.java
+++ src/main/java/com/priorityslots/PrioritySlotsConfig.java
@@ -2,18 +2,9 @@ package com.priorityslots;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
-import net.runelite.client.config.ConfigItem;
 
-@ConfigGroup("example")
+@ConfigGroup(PrioritySlotsConfig.GROUP)
 public interface PrioritySlotsConfig extends Config
 {
-	@ConfigItem(
-		keyName = "greeting",
-		name = "Welcome Greeting",
-		description = "The message to show to the user when they login"
-	)
-	default String greeting()
-	{
-		return "Hello";
-	}
+	String GROUP = "priorityslots";
 }
--- src/main/java/com/priorityslots/PrioritySlotsPlugin.java
+++ src/main/java/com/priorityslots/PrioritySlotsPlugin.java
@@ -1,48 +1,34 @@
 package com.priorityslots;
 
 import com.google.inject.Provides;
-import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
-import net.runelite.api.Client;
-import net.runelite.api.GameState;
-import net.runelite.api.events.GameStateChanged;
 import net.runelite.client.config.ConfigManager;
-import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+		name = "Priority Slots",
+		description = "Dynamic bank-layout slots that display the highest-priority item you currently own.",
+		tags = {
+				"bank",
+				"layout",
+				"gear",
+				"priority"
+		}
 )
 public class PrioritySlotsPlugin extends Plugin
 {
-	@Inject
-	private Client client;
-
-	@Inject
-	private PrioritySlotsConfig config;
-
 	@Override
-	protected void startUp() throws Exception
+	protected void startUp()
 	{
-		log.debug("Example started!");
+		log.debug("Priority Slots started");
 	}
 
 	@Override
-	protected void shutDown() throws Exception
+	protected void shutDown()
 	{
-		log.debug("Example stopped!");
-	}
-
-	@Subscribe
-	public void onGameStateChanged(GameStateChanged gameStateChanged)
-	{
-		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
-		{
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
-		}
+		log.debug("Priority Slots stopped");
 	}
 
 	@Provides
