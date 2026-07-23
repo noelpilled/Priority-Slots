c9dca311593fe5769682cea05e26720f21f053d1
noel <mail@noel.run>
2026-07-23 00:13:59 -0700

    feat: load priority state with plugin lifecycle

--- src/main/java/com/priorityslots/PrioritySlotsPlugin.java
+++ src/main/java/com/priorityslots/PrioritySlotsPlugin.java
@@ -2,10 +2,14 @@ package com.priorityslots;
 
 import com.priorityslots.bank.BankSnapshotFactory;
 import com.priorityslots.domain.BankSnapshot;
+import com.priorityslots.domain.PriorityState;
+import com.priorityslots.persistence.PriorityStateStore;
+import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.events.ItemContainerChanged;
 import net.runelite.api.gameval.InventoryID;
 import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.events.ProfileChanged;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 
@@ -25,20 +29,37 @@ public class PrioritySlotsPlugin extends Plugin
 	private final BankSnapshotFactory bankSnapshotFactory =
 			new BankSnapshotFactory();
 
+	@Inject
+	private PriorityStateStore priorityStateStore;
+
+	private PriorityState priorityState =
+			PriorityState.empty();
+
 	private BankSnapshot bankSnapshot =
 			BankSnapshot.empty();
 
 	@Override
 	protected void startUp()
 	{
+		reloadPriorityState();
+
 		log.debug("Priority Slots started");
 	}
 
 	@Override
 	protected void shutDown()
 	{
+		priorityState = PriorityState.empty();
 		bankSnapshot = BankSnapshot.empty();
+
 		log.debug("Priority Slots stopped");
+	}
+
+	@Subscribe
+	public void onProfileChanged(
+			ProfileChanged profileChanged)
+	{
+		reloadPriorityState();
 	}
 
 	@Subscribe
@@ -57,6 +78,20 @@ public class PrioritySlotsPlugin extends Plugin
 		log.debug(
 				"Captured bank snapshot with {} exact item IDs",
 				bankSnapshot.distinctItemCount()
+		);
+	}
+
+	private void reloadPriorityState()
+	{
+		priorityState = priorityStateStore.load();
+
+		log.debug(
+				"Loaded Priority Slots state with "
+						+ "{} definitions, {} groups, "
+						+ "and {} bank tag bindings",
+				priorityState.getDefinitions().size(),
+				priorityState.getGroups().size(),
+				priorityState.getBindings().size()
 		);
 	}
 }
